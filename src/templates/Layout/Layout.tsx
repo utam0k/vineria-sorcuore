324c266c8b024aa8ddba206484ff4ee134d332a2
asakura-dev <asakuradev@gmail.com>
2020-05-02 20:19:36 +0900

    Add Head component

--- src/templates/Layout/Layout.tsx
+++ src/templates/Layout/Layout.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import Head from 'next/head';
+import { Head } from '../../components/Head/Head';
 import { Header } from '../../organisms/Header/Header';
 
 type Props = React.ComponentProps<typeof Header>;
 
 export const Layout: React.FC<Props> = ({ children, ...props }) => (
   <>
-    <Head>
-      <title>Vineria Sorcuor</title>
-      <meta
-        name="description"
-        content="イタリア語で SORRISO (笑顔) CUORE (心)。かけ合わせて作った造語それがソルクオーレです。\n心を込めて笑顔になれる楽しい本格イタリアンをお客様にお届けします。ぜひ、素敵な時間をお過ごしください。"
-      ></meta>
-    </Head>
+    <Head
+      title="Vineria Sorcuor"
+      description="イタリア語で SORRISO (笑顔) CUORE (心)。かけ合わせて作った造語それがソルクオーレです。心を込めて笑顔になれる楽しい本格イタリアンをお客様にお届けします。ぜひ、素敵な時間をお過ごしください。"
+      keywords={['Vineria Sorcuor', 'ヴィネリア ソルクオーレ', '両国', 'イタリアン']}
+      imageUrl="https://jimdo-storage.freetls.fastly.net/image/59782918/5404c779-15aa-4d6d-91bb-f4083dbc737d.jpg"
+    />
     <>
       <Header {...props} />
       {children}
