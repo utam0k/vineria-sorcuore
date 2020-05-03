4cd0c163f0618688c08ff8acbf0a1f7a9e669e1c
utam0k <k0ma@utam0k.jp>
2020-05-04 01:07:37 +0900

    storybook pages.

---
+++ src/story-pages/store-infomation.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import StoreInfomation from '../pages/store-infomation';
+
+export default { title: 'StoreInfomation' };
+
+export const StoreInfomationPage = () => <StoreInfomation />;
