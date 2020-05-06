f0cb0b205c26da3be3379086d628a077315543dd
utam0k <k0ma@utam0k.jp>
2020-05-06 20:25:02 +0900

    top page of menu.

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,73 +1,104 @@
-import React, { useState, useCallback } from 'react';
-import { Heading, Flex, Text, Box, useTheme } from '@chakra-ui/core';
-import { Card } from '../../components/Card/Card';
-import { CardSkeleton } from '../../components/Card/CardSkeleton';
-import { ImageModal } from '../../molecules/ImageModal/ImageModal';
+import React, { Fragment } from 'react';
+import { Link, Button, SimpleGrid, Text, Heading, Flex, Box, useTheme } from '@chakra-ui/core';
+
 import { Layout } from '../../templates/Layout/Layout';
-import { useWindowSize } from '../../utils/hooks/useWindowSize';
 
-import useSWR from 'swr';
-import fetch from 'unfetch';
-
-import { Menu as TMenu } from '../../models/menu';
-
-const fetcher = (url: string) => fetch(url).then((r) => r.json());
-
-const Menu = () => {
+export const MenuTop = () => {
   const theme = useTheme();
-  const windowSize = useWindowSize();
-
-  const { data: menus } = useSWR<TMenu[]>(process.env.menuUrl || '', fetcher);
-
-  const [imageModal, setImageModal] = useState<{ isOpen: boolean; title: string; imageUrl: string }>({
-    isOpen: false,
-    title: '',
-    imageUrl: '',
-  });
-
-  const onCardClick = useCallback((title: string, imageUrl: string) => {
-    setImageModal({ isOpen: true, title, imageUrl });
-  }, []);
-
-  const onModalClose = useCallback(() => {
-    setImageModal({ isOpen: false, title: '', imageUrl: '' });
-  }, []);
-
-  const NumCardsInOneLine = (width: number) => {
-    // TODO: get max width of a Card component automatically.
-    const cardMaxWidth = 384;
-    return cardMaxWidth > width ? 1 : Math.floor(width / cardMaxWidth);
-  };
-
   return (
     <Layout>
-      <Box as="header" textAlign="center">
-        <Heading as="h1" marginTop={theme.space[8]}>
-          ソルクオーレの食卓
-        </Heading>
-        <Text marginBottom={theme.space[12]} marginTop={theme.space[8]} whiteSpace="pre-wrap">
-          {`テイクアウトでお手軽にソルクオーレの本格イタリアンを楽しめるシリーズです！\n夜ご飯のおかずに困ったとき、お酒のお供に。`}
-        </Text>
-      </Box>
-      <Flex align="center" justify="center" flexWrap="wrap">
-        {menus
-          ? menus.map((menu, i) => (
-              <Card
-                title={menu.name}
-                description={menu.description}
-                price={menu.price}
-                imageUrl={menu.image}
-                key={i}
-                onClick={onCardClick}
-              />
-            ))
-          : [...new Array(windowSize.width ? NumCardsInOneLine(windowSize.width) : 1)].map((_, i) => (
-              <CardSkeleton key={i} />
+      <Heading
+        textAlign="center"
+        fontSize={[theme.fontSizes['3xl'], theme.fontSizes['3xl'], theme.fontSizes['3xl'], theme.fontSizes['5xl']]}
+        marginY={[theme.space[4], theme.space[4], theme.space[4], theme.space[8]]}
+        fontWeight="normal"
+      >
+        メニュー
+      </Heading>
+      <Flex
+        paddingY={[theme.space[0], theme.space[0], theme.space[0], theme.space[4]]}
+        justify="center"
+        flexWrap={['wrap', 'wrap', 'wrap', 'nowrap']}
+        paddingX={theme.space[1]}
+      >
+        <Box
+          maxWidth={theme.sizes['4xl']}
+          marginBottom={[theme.space[8], theme.space[8], theme.space[8], 0]}
+          marginTop={theme.space[4]}
+          marginRight={[theme.space[8], theme.space[8], theme.space[8], theme.space[16]]}
+          marginLeft={[theme.space[8], theme.space[8], theme.space[8], 0]}
+        >
+          <SimpleGrid columns={[1, 1, 1, 2]} spacingX={6} spacingY={[0, 0, 0, 12]}>
+            {[
+              {
+                name: 'コース',
+                desc:
+                  '旬の食材、イタリア直送の食材を使ったフルコースのご用意です。パスタとメインディッシュがプリフィックスとなっており、メインディッシュはご予約の際にお伺いいたします。お任せコースも承ります。\n※コースは予約のみ',
+                button: {
+                  label: '詳細を見る',
+                  link: '/menu/course',
+                },
+              },
+              {
+                name: 'アラカルト',
+                desc: 'ワインに合う小皿のおつまみから、本格的なイタリアの郷土料理まで豊富に揃えております。',
+                button: {
+                  label: '詳細を見る',
+                  link: 'menu/a-la-carte',
+                },
+              },
+              {
+                name: 'テイクアウト',
+                desc:
+                  'テイクアウトでお手軽にソルクオーレの本格イタリアンを楽しめるシリーズです！夜ご飯のおかずに困ったとき、お酒のお供に。',
+                button: {
+                  label: '詳細を見る',
+                  link: 'menu/takeout',
+                },
+              },
+            ].map((item, i) => (
+              <Fragment key={i}>
+                <Flex borderRight={['none', 'none', 'none', 'solid']}>
+                  <Heading
+                    fontSize={['lg', 'lg', 'xl', 'xl']}
+                    fontWeight={['2xl', '2xl', '2xl', 'md']}
+                    width="100%"
+                    borderBottom={['solid', 'solid', 'solid', 'none']}
+                    marginBottom={[theme.space[2], theme.space[2], 0, 0]}
+                    textAlign={['left', 'left', 'center', 'center']}
+                  >
+                    {item.name}
+                  </Heading>
+                </Flex>
+                <Box marginBottom={[theme.space[6], theme.space[6], theme.space[6], 0]}>
+                  <Text
+                    marginLeft={[theme.space[4], theme.space[4], theme.space[4], 0]}
+                    whiteSpace="pre-wrap"
+                    overflowWrap="break-word"
+                    wordBreak="break-all"
+                  >
+                    {item.desc}
+                  </Text>
+                  <Flex marginTop={theme.space[4]} align="center" justify="center">
+                    <Link
+                      href={item.button.link}
+                      _hover={{
+                        textDecoration: 'none',
+                      }}
+                    >
+                      <Button variantColor="gray" variant="outline">
+                        {item.button.label}
+                      </Button>
+                    </Link>
+                  </Flex>
+                </Box>
+              </Fragment>
             ))}
+          </SimpleGrid>
+        </Box>
       </Flex>
-      <ImageModal {...imageModal} onClose={onModalClose} />
     </Layout>
   );
 };
 
-export default Menu;
+export default MenuTop;
