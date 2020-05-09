48ec229e69e9bd35db8b5ac96f304fc576957674
succie <kodai.suyama@gmail.com>
2020-05-09 10:38:11 +0900

    fix takeout menu

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useCallback } from 'react';
-import { Text, Badge, Flex, Box, Image, useTheme } from '@chakra-ui/core';
+import { Text, Badge, Flex, Box, Image, useTheme, BoxProps } from '@chakra-ui/core';
 
 type Props = {
   imageUrl: string;
@@ -15,19 +15,30 @@ type Props = {
     isFrozen: boolean;
   };
   onClick: (title: string, imageUrl: string) => void;
-};
+} & Omit<BoxProps, 'onClick'>;
 
-export const Card: React.FC<Props> = ({ imageUrl, imageAlt, title, cost, description, badges, onClick }) => {
+export const Card: React.FC<Props> = ({ imageUrl, imageAlt, title, cost, description, badges, onClick, ...props }) => {
   const theme = useTheme();
 
   const onCardClick = useCallback(() => {
     onClick(title, imageUrl);
   }, [title, imageUrl, onClick]);
 
+  const deliveryBadge = useMemo(() => {
+    if (badges.isDeliverable) {
+      return (
+        <Badge rounded="full" variant="outline" marginRight={theme.space[1]} paddingX="2" variantColor="orange">
+          地方発送可
+        </Badge>
+      );
+    }
+    return null;
+  }, [badges.isDeliverable]);
+
   const frozenBadge = useMemo(() => {
     if (badges.isFrozen) {
       return (
-        <Badge marginX={theme.space[1]} rounded="full" variant="outline" px="2" variantColor="teal">
+        <Badge rounded="full" variant="outline" marginRight={theme.space[1]} paddingX="2" variantColor="teal">
           冷凍真空パック済
         </Badge>
       );
@@ -46,30 +57,19 @@ export const Card: React.FC<Props> = ({ imageUrl, imageAlt, title, cost, descrip
     );
   }, [cost]);
 
-  const deliveryBadge = useMemo(() => {
-    if (badges.isDeliverable) {
-      return (
-        <Badge marginX={theme.space[1]} rounded="full" variant="outline" px="2" variantColor="orange">
-          地方発送可
-        </Badge>
-      );
-    }
-    return null;
-  }, [badges.isDeliverable]);
-
   return (
     <Box
-      maxW="sm"
+      maxWidth={[theme.sizes.xs, theme.sizes.xs, theme.sizes.sm, theme.sizes.sm]}
       borderWidth="1px"
-      margin="auto"
       overflow="hidden"
       rounded="lg"
       cursor="pointer"
       onClick={onCardClick}
       marginBottom={theme.space[4]}
+      {...props}
     >
       <Image alt={imageAlt ? imageAlt : title} src={imageUrl} width="sm" height="2xs" objectFit="cover" />
-      <Box p="6" height={theme.sizes[40]}>
+      <Box padding={theme.space[6]} height={theme.sizes[40]}>
         <Flex height="18px">
           {deliveryBadge}
           {frozenBadge}
