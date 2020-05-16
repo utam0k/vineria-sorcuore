126a832f1f3656491699942fbcedc226eba1319a
utam0k <k0ma@utam0k.jp>
2020-05-16 15:14:43 +0900

    remove about section in top page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,17 +48,6 @@ export const Home = () => {
     <Layout position="fixed" needGradation>
       <Description />
       <Section
-        title="About"
-        text={
-          '初めは千葉県松戸市の「オステリア タナロ」から始まりました。\nイタリア滞在経験がありイタリアをこよなく愛するオーナー兼ソムリエの成嶋とシェフの神谷の熱い思いによって皆様に愛されるお店になり、そして２号店となる「ヴィネリア ソルクオーレ」を両国にオープンする事が出来ました。\n当店ではイタリア滞在経験のあるシェフ神谷がお料理をご提供します。'
-        }
-        image={{
-          url:
-            'https://lh3.googleusercontent.com/JWJSzuD1xA0woE-TFL4kQOmn7otjqVslrQ4uOFTyAJSUqr5LYzgfPK_tJWieGRodPHgn_iljweQPG8hNdiqYSgaNXii7X2YcEIgeYxQRytqHle8LUuvwxz2f8sCCVO7cbQoXVX1YMw',
-          alt: 'about',
-        }}
-      />
-      <Section
         title="Concept"
         text={
           '住宅街にひっそりと佇むイタリアンレストラン。\n広々としたカウンターのお席で時間を忘れてワインとお食事をお楽しみいただけます。'
@@ -68,37 +57,7 @@ export const Home = () => {
             'https://jimdo-storage.freetls.fastly.net/image/58541700/a859a20a-51e0-411d-a2f0-68d07866595a.jpg?format=pjpg&quality=80&auto=webp&disable=upscale',
           alt: 'concept',
         }}
-        reverse
       />
-      <Section
-        title="Lunch"
-        text={
-          'イタリアで修行を重ねたシェフによる、イタリアンビュッフェ。（お持ち帰り弁当も可）イタリアの塩、エクストラヴァージンオイルで調理したオードブルとパスタが１０種類以上！！\nテイクアウト弁当もご用意しておりますのでお仕事の休憩中にもオススメです。（セルフサービス）イートインも設けております。'
-        }
-        image={{
-          url:
-            'https://jimdo-storage.freetls.fastly.net/image/75731262/55fee928-e220-4128-90c1-f7b19544ce20.jpg?format=pjpg&quality=80&auto=webp&disable=upscale',
-          alt: 'lunch',
-        }}
-      >
-        <Box marginTop={theme.space[8]}>
-          {[
-            { name: 'ブッフェ45分', price: '1,100' },
-            { name: 'ブッフェ75分', price: '1,650' },
-            { name: '詰め放題・お持ち帰り弁当', price: '1,100' },
-            { name: '飲み放題ワイン45分', price: '1,000' },
-          ].map((item) => (
-            <Fragment key={item.name}>
-              <Flex justify="space-between" marginTop={theme.space[1]}>
-                <Heading fontSize="md" fontWeight="md">
-                  {item.name}
-                </Heading>
-                <Text fontSize="md">{item.price}円</Text>
-              </Flex>
-            </Fragment>
-          ))}
-        </Box>
-      </Section>
       <Section
         title="Dinner"
         text={
@@ -129,6 +88,60 @@ export const Home = () => {
               </Button>
             </Link>
           ))}
+        </Flex>
+      </Section>
+      <Section
+        title="Lunch"
+        text={
+          'イタリアで修行を重ねたシェフによる、イタリアンビュッフェ。（お持ち帰り弁当も可）イタリアの塩、エクストラヴァージンオイルで調理したオードブルとパスタが１０種類以上！！\nテイクアウト弁当もご用意しておりますのでお仕事の休憩中にもオススメです。（セルフサービス）イートインも設けております。'
+        }
+        image={{
+          url:
+            'https://jimdo-storage.freetls.fastly.net/image/75731262/55fee928-e220-4128-90c1-f7b19544ce20.jpg?format=pjpg&quality=80&auto=webp&disable=upscale',
+          alt: 'lunch',
+        }}
+      >
+        <Box marginTop={theme.space[8]}>
+          {[
+            { name: 'ブッフェ45分', price: '1,100' },
+            { name: 'ブッフェ75分', price: '1,650' },
+            { name: '詰め放題・お持ち帰り弁当', price: '1,100' },
+            { name: '飲み放題ワイン45分', price: '1,000' },
+          ].map((item) => (
+            <Fragment key={item.name}>
+              <Flex justify="space-between" marginTop={theme.space[1]}>
+                <Heading fontSize="md" fontWeight="md">
+                  {item.name}
+                </Heading>
+                <Text fontSize="md">{item.price}円</Text>
+              </Flex>
+            </Fragment>
+          ))}
+        </Box>
+      </Section>
+      <Section
+        title="Takeout"
+        text={
+          'ソルクオーレの食卓は、テイクアウトでお手軽にソルクオーレの本格イタリアンを楽しめるシリーズです！\n夜ご飯のおかずに困ったとき、お酒のお供に。'
+        }
+        image={{
+          url:
+            'https://lh3.googleusercontent.com/ZPVP_2WB3wQR7xxGJ3__g56T0Ah0_LhBV42WWUqI7wnxKMk8XLrng1JI7bbyTMydPXcu1bFy5xlVoCUb1Ybb1eA82cCsdQLSPstCfndcS9N1WHv_Z2yy0U0cpDE8Y5NiRXbXUyXwgA',
+          alt: 'takeout',
+        }}
+        reverse
+      >
+        <Flex justify="center" alignItems="center" marginTop={theme.space[8]}>
+          <Link
+            href={'/menu/takeout'}
+            _hover={{
+              textDecoration: 'none',
+            }}
+          >
+            <Button variantColor="yellow" variant="outline">
+              メニューを見る
+            </Button>
+          </Link>
         </Flex>
       </Section>
       <Section
@@ -201,28 +214,36 @@ export const Home = () => {
         </Tabs>
       </Section>
       <Section
-        title="Takeout"
-        text={
-          'ソルクオーレの食卓は、テイクアウトでお手軽にソルクオーレの本格イタリアンを楽しめるシリーズです！\n夜ご飯のおかずに困ったとき、お酒のお供に。'
-        }
+        title="SNS"
+        text={'店舗からの最新のお知らせや、料理の写真などを掲載しています。よかったら覗いてみてください。'}
         image={{
           url:
-            'https://lh3.googleusercontent.com/ZPVP_2WB3wQR7xxGJ3__g56T0Ah0_LhBV42WWUqI7wnxKMk8XLrng1JI7bbyTMydPXcu1bFy5xlVoCUb1Ybb1eA82cCsdQLSPstCfndcS9N1WHv_Z2yy0U0cpDE8Y5NiRXbXUyXwgA',
-          alt: 'takeout',
+            'https://lh3.googleusercontent.com/YhdX0cVoH1I8NtF-4ZdLngPyBsxQmoUg_fDlT0B4Ah3NtfQRDPkl6s5wvh_PO-RvlmjdXDXsDjuRs93-wB8KRuxBgaNb4wGgHjgLYV96jNv3DSOV0hnGNRu3d1s7KNMdaVvBJtKPxKSa54qflvrXWoRAGlzszpxZHjP4dtBUq0i2X4W5brOVt0S5eAm14qk4UnSpvfE6tkqGnKbkg6EEPaPIB-YOrLjTVDstU8r3A8gkK4uuuCBIWLxjR0nPQofBDaeXpm0Gb3-qwdZpA36fJQKsFfLW1SAH9uA9zJqKaHxi81Uz6ZDZWNMCppt3spx6-Rzs6TtrIL4QrRvpPSThacYwJU9ZlLsJezi28GsITfFGpnmhAKHAxEm4J0LXZHorU89Yz0kBnOd1lwQ6kJrdKKcdlMat1zL58p-Nmf7FGLmgWlZ8kW2tv4wvW5aL9yopUa94Z6YatZGsJgMObj-vCHVzDjNI_Rm7v9aAzNlHTbMST9l6qZgAm1kQV1raakQGrPOtrNNIJUm_AizLFUhFyBGgg9mkU5kGkARhsbaGj3MlTZ77LP0-cGxXpKXCpG9Xto_euKxoVHW1d4HpiTnZzyNDqW7RhCQix8Ykh5gZHIhIOVzU4WTBZDEV9hSzZSduiopIxChsxjcw4Fx1dhi_Mpxz8kqgqkBiQ8b4i7ANQZhny1VJJ_k_0Udzik0B_YKPuo-MOhM75bRqrZ616hKPhLstYtUwOPE2SVJgYV9FcD7LHXeEgtTM5g',
+          alt: 'sns',
         }}
         reverse
       >
-        <Flex justify="center" alignItems="center" marginTop={theme.space[8]}>
-          <Link
-            href={'/menu/takeout'}
-            _hover={{
-              textDecoration: 'none',
-            }}
-          >
-            <Button variantColor="yellow" variant="outline">
-              メニューを見る
-            </Button>
-          </Link>
+        <Flex align="center" justify="center" flexWrap="wrap" marginTop={theme.space[4]}>
+          {[
+            { name: 'Instagram', href: 'https://www.instagram.com/vineriasorcuore1213/', icon: faInstagram },
+            { name: 'Facebook', href: 'https://www.facebook.com/vineriasorcuore1213/', icon: faFacebook },
+          ].map((item) => (
+            <Link
+              href={item.href}
+              marginX={theme.space[2]}
+              marginBottom={theme.space[4]}
+              key={item.name}
+              _hover={{ textDecoration: 'none' }}
+              isExternal
+            >
+              <Button>
+                <FontAwesomeIcon icon={item.icon} />
+                <Text marginLeft={theme.space[2]} fontSize={theme.fontSizes.sm}>
+                  {item.name}
+                </Text>
+              </Button>
+            </Link>
+          ))}
         </Flex>
       </Section>
       <Section
@@ -277,39 +298,6 @@ export const Home = () => {
             </Fragment>
           ))}
         </SimpleGrid>
-      </Section>
-      <Section
-        title="SNS"
-        text={'店舗からの最新のお知らせや、料理の写真などを掲載しています。よかったら覗いてみてください。'}
-        image={{
-          url:
-            'https://lh3.googleusercontent.com/YhdX0cVoH1I8NtF-4ZdLngPyBsxQmoUg_fDlT0B4Ah3NtfQRDPkl6s5wvh_PO-RvlmjdXDXsDjuRs93-wB8KRuxBgaNb4wGgHjgLYV96jNv3DSOV0hnGNRu3d1s7KNMdaVvBJtKPxKSa54qflvrXWoRAGlzszpxZHjP4dtBUq0i2X4W5brOVt0S5eAm14qk4UnSpvfE6tkqGnKbkg6EEPaPIB-YOrLjTVDstU8r3A8gkK4uuuCBIWLxjR0nPQofBDaeXpm0Gb3-qwdZpA36fJQKsFfLW1SAH9uA9zJqKaHxi81Uz6ZDZWNMCppt3spx6-Rzs6TtrIL4QrRvpPSThacYwJU9ZlLsJezi28GsITfFGpnmhAKHAxEm4J0LXZHorU89Yz0kBnOd1lwQ6kJrdKKcdlMat1zL58p-Nmf7FGLmgWlZ8kW2tv4wvW5aL9yopUa94Z6YatZGsJgMObj-vCHVzDjNI_Rm7v9aAzNlHTbMST9l6qZgAm1kQV1raakQGrPOtrNNIJUm_AizLFUhFyBGgg9mkU5kGkARhsbaGj3MlTZ77LP0-cGxXpKXCpG9Xto_euKxoVHW1d4HpiTnZzyNDqW7RhCQix8Ykh5gZHIhIOVzU4WTBZDEV9hSzZSduiopIxChsxjcw4Fx1dhi_Mpxz8kqgqkBiQ8b4i7ANQZhny1VJJ_k_0Udzik0B_YKPuo-MOhM75bRqrZ616hKPhLstYtUwOPE2SVJgYV9FcD7LHXeEgtTM5g',
-          alt: 'sns',
-        }}
-        reverse
-      >
-        <Flex align="center" justify="center" flexWrap="wrap" marginTop={theme.space[4]}>
-          {[
-            { name: 'Instagram', href: 'https://www.instagram.com/vineriasorcuore1213/', icon: faInstagram },
-            { name: 'Facebook', href: 'https://www.facebook.com/vineriasorcuore1213/', icon: faFacebook },
-          ].map((item) => (
-            <Link
-              href={item.href}
-              marginX={theme.space[2]}
-              marginBottom={theme.space[4]}
-              key={item.name}
-              _hover={{ textDecoration: 'none' }}
-              isExternal
-            >
-              <Button>
-                <FontAwesomeIcon icon={item.icon} />
-                <Text marginLeft={theme.space[2]} fontSize={theme.fontSizes.sm}>
-                  {item.name}
-                </Text>
-              </Button>
-            </Link>
-          ))}
-        </Flex>
       </Section>
     </Layout>
   );
